9f61eb0f8a53ab9c0d255ff63ef5bbfafc9c4728
TILTL <131395335+TILTL@users.noreply.github.com>
2024-08-19 20:46:07 +0800

    Add files via upload

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+  title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService, private router: Router) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+    // Unsubscribe to ensure no memory leaks
+    this.subscription.unsubscribe();
+  }
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
+
+}
